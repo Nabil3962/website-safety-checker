679d4b7f04a3735e2ebd111198998c47282058fe
Suhail Al Mehedi <156921559+Nabil3962@users.noreply.github.com>
2026-05-08 23:12:04 +0600

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ from detector import analyze_url
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET"])
+@app.route("/")
 def home():
     return render_template("index.html")
 
 
-@app.route("/analyze", methods=["POST"])
-def analyze():
+@app.route("/check", methods=["POST"])
+def check():
     data = request.get_json()
     url = data.get("url")
 
@@ -22,4 +22,6 @@ def analyze():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
